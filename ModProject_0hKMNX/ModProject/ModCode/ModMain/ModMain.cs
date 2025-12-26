c130126951b436d0024707edef067c1bb914c30b
Huỳnh Chí Phong <hiroshi.kaze1994@gmail.com>
2025-12-26 11:22:39 +0700

    move sample project

---
+++ ModProject_0hKMNX/ModProject/ModCode/ModMain/ModMain.cs
@@ -0,0 +1,13 @@
+﻿using ModLib.Enum;
+using ModLib.Mod;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace ModCreatorChildEvent
+{
+    [Cache("#PROJECTID#", CacheType = CacheAttribute.CType.Global, OrderIndex = 0)]
+    public class ModMain : ModChild
+    {
+    }
+}
